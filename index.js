92a8481229543fcfde3f9ffe53d102e8b8eac14a
Roman Sachenko <roman.sachenko@gmail.com>
2018-08-19 20:39:12 +0300

    feat: API factory added

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Apache Fineract API 
+ * 
+ * Wraps Apache Fineract API. 
+ * Provides API Versioning. V1 is only available version for now.
+ * V2 can be used as an example.
+ */
+module.exports = require('./lib/');

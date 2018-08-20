fcb3bd304dfddb57504772465fadb5126dc523f8
Roman Sachenko <roman.sachenko@gmail.com>
2018-08-21 00:09:51 +0300

    feat: test moved

---
+++ test/unit/src/dependencies.js
@@ -0,0 +1,15 @@
+module.exports = {
+  services: {
+    ApiRequestService: require('../../../lib/services/ApiRequestService'),
+  },
+  api: {
+    v1: {
+      Client: require('../../../lib/api/v1/resources/Client'),
+      Loan: require('../../../lib/api/v1/resources/Loan'),
+      LoanProduct: require('../../../lib/api/v1/resources/LoanProduct'),
+      Resource: require('../../../lib/api/v1/Resource'),
+    },
+  },
+  config: require('../config.json'),
+  testData: require('./helpers/testData'),
+};
